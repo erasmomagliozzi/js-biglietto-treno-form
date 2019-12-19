59fac8b6557ae74dee8fcae5486fdd8963f1838f
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2019-12-19 18:07:11 +0100

    wip

--- js/biglietto-treno.js
+++ js/biglietto-treno.js
@@ -18,23 +18,47 @@ buttonGenera.addEventListener('click',
    console.log(kmViaggio);
    // prezzo totale del viaggio per persone tra i 19 e 64 anni
    // 0.21€ al km
-   var prezzoBiglietto = kmViaggio * 0.21;
-   console.log(prezzoBiglietto);
+   var costoBiglietto = kmViaggio * 0.21;
+   console.log(costoBiglietto);
+
+   var offerta = 'Tariffa Standard'
    // sconto
    // 20% under 18
    // 40% over 65
    if (fascia == 'minorenne'){
-     var calcoloScontoUnder = ((prezzoBiglietto * 20) / 100 );
-     var bigliettoUnder = prezzoBiglietto - calcoloScontoUnder;
-     console.log(bigliettoUnder);
+     var calcoloScontoUnder = ((costoBiglietto * 20) / 100 );
+     var costoBiglietto = costoBiglietto - calcoloScontoUnder;
+
+     offerta = 'Sconto Minorenne'
    }else if (fascia == 'over 65'){
-     var calcoloScontoOver = ((prezzoBiglietto * 40) / 100 );
-     var bigliettoOver = prezzoBiglietto - calcoloScontoOver;
-     console.log(bigliettoOver);
+     var calcoloScontoOver = ((costoBiglietto * 40) / 100 );
+     var costoBiglietto = costoBiglietto - calcoloScontoOver;
+
+     offerta = 'Sconto Over 65'
    }
+  var carrozza = Math.floor(Math.random()*9) + 1;
+  var cp = Math.floor(Math.random() * (100000 - 90000 + 1)) +90000;
+
 
    document.getElementById('nome_passeggero').innerHTML = nome;
+   document.getElementById('offerta').innerHTML = offerta;
+   document.getElementById('costo').innerHTML = costoBiglietto.toFixed(2);
+   document.getElementById('codice-cp').innerHTML = cp;
+   document.getElementById('carrozza').innerHTML = carrozza;
 
 }
+);
+var buttonAnnulla = document.getElementById('button_annulla');
+buttonAnnulla.addEventListener('click',
+ function(){
+   document.getElementById('nome_passeggero').innerHTML = "";
+   document.getElementById('offerta').innerHTML = "";
+   document.getElementById('costo').innerHTML = "";
+   document.getElementById('codice-cp').innerHTML = "";   document.getElementById('carrozza').innerHTML = "";
 
-)
+   document.getElementById('nome').value = "";
+   document.getElementById('fascia').value = "";
+   document.getElementById('km').value = "";
+ }
+
+);
